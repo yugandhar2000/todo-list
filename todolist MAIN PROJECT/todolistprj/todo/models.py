fc42dd9d964b279752698b04fb72a0d8ba68d504
yugandhar <ravuruyugandhar777@gmail.com>
2022-01-12 22:46:53 +0530

    first

---
+++ todolist MAIN PROJECT/todolistprj/todo/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    def get_description(self):
+        return self.description
+    def get_complete(self):
+        return self.complete
+    def get_created(self):
+        return self.created
+
+    class Meta:
+        ordering = ['complete']
